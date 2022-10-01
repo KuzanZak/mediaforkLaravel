72b50f4466d1e323fb9bce64590ce567a8a53fda
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-10-01 22:36:22 +0200

    css on customers dashboard ok

--- resources/views/dashboard-customer.blade.php
+++ resources/views/dashboard-customer.blade.php
@@ -1,19 +1,16 @@
 @extends('dashboard')
 @section('content')
-<div class="p-6 bg-white border-b border-gray-200">
+<div class="title-dashboard">
     Customers
 </div>
-<ul>
+<ul class="list-dashboard">
     @foreach($customers as $customer)
-    <li>
-        <p>Id : {{ $customer->id }}</p>
-        <p>Nom : {{ $customer->name }}
-        <p>
-        <p>Email : {{ $customer->email }}
-        <p>
-        <p>Message : {{ $customer->message }}
-        <p>
-            <br>
+    <li class="list-items-dashboard">
+        <p class="data-dashboard"><span class="span-title-dashboard">Id :</span> {{ $customer->id }}</p>
+        <p class="data-dashboard"><span class="span-title-dashboard">Nom :</span> {{ $customer->name }}</p>
+        <p class="data-dashboard"><span class="span-title-dashboard">Email :</span> {{ $customer->email }}</p>
+        <p class="data-dashboard"><span class="span-title-dashboard">Message :</span> {{ $customer->message }}</p>
+        <br>
     </li>
     @endforeach
 </ul>
