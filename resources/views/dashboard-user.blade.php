fe99890655d7ed95c7c3927d32d04c1fda87a3a3
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-10-01 23:26:23 +0200

    css user dashboard, 2 list (1x admin/1x user) ok

--- resources/views/dashboard-user.blade.php
+++ resources/views/dashboard-user.blade.php
@@ -14,26 +14,45 @@
 <h2 class="title-dashboard">
     Users
 </h2>
-<ul class="list-dashboard">
-    @foreach($users as $user)
-    <li class="list-items-dashboard">
-        <p class="data-dashboard"><span class="span-title-dashboard">Id :</span> {{ $user->id }}</p>
-        <p class="data-dashboard"><span class="span-title-dashboard">Nom :</span> {{ $user->name }}</p>
-        <p class="data-dashboard"><span class="span-title-dashboard">Email :</span> {{ $user->email }}</p>
-        <p class="data-dashboard"><span class="span-title-dashboard">Admin :</span> {{ $user->admin }}
-            @if ($user->admin !== 1 && $user->id !== Auth::id())
-            <a href="{{ @route('dashboard/updateAdmin', $user->id)}}">
-                <i class=" fa fa-check-square" aria-hidden="true"></i>
-            </a>
-            @endif
-            @if ($user->admin === 1 && $user->id !== Auth::id())
-            <a href="{{ @route('dashboard/updateUser', $user->id)}}">
-                <i class="fa fa-minus" aria-hidden="true"></i>
-            </a>
-            @endif
-        </p>
-        <br>
-    </li>
-    @endforeach
-</ul>
+<section class="list-dashboard-users">
+    <ul class="list-dashboard-admin">
+        @foreach($users as $user)
+        @if ($user->admin === 1)
+        <li class="list-items-dashboard">
+            <p class="data-dashboard"><span class="span-title-dashboard">Id :</span> {{ $user->id }}</p>
+            <p class="data-dashboard"><span class="span-title-dashboard">Nom :</span> {{ $user->name }}</p>
+            <p class="data-dashboard"><span class="span-title-dashboard">Email :</span> {{ $user->email }}</p>
+            <p class="data-dashboard"><span class="span-title-dashboard">Admin :</span> {{ $user->admin }}
+                @if ($user->id !== Auth::id())
+                <a href="{{ @route('dashboard/updateUser', $user->id)}}">
+                    <i class="fa fa-minus" aria-hidden="true"></i>
+                </a>
+                @endif
+            </p>
+            <br>
+        </li>
+        @endif
+        @endforeach
+    </ul>
+    <ul class="list-dashboard">
+        @foreach($users as $user)
+        @if ($user->admin !== 1)
+        <li class="list-items-dashboard">
+            <p class="data-dashboard"><span class="span-title-dashboard">Id :</span> {{ $user->id }}</p>
+            <p class="data-dashboard"><span class="span-title-dashboard">Nom :</span> {{ $user->name }}</p>
+            <p class="data-dashboard"><span class="span-title-dashboard">Email :</span> {{ $user->email }}</p>
+            <p class="data-dashboard"><span class="span-title-dashboard">Admin :</span> {{ $user->admin }}
+                @if ($user->id !== Auth::id())
+                <a href="{{ @route('dashboard/updateAdmin', $user->id)}}">
+                    <i class=" fa fa-check-square" aria-hidden="true"></i>
+                </a>
+                @endif
+            </p>
+            <br>
+        </li>
+        @endif
+        @endforeach
+    </ul>
+</section>
+
 @endsection
